deed02206fe108049faaf361c40e79c72efc97a2
MatildeCrxtt <54672139+MatildeCrxtt@users.noreply.github.com>
2020-06-18 20:33:42 -0400

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,14 @@
+  <div class="clear"></div>
+
+</div><!--//main_container-->
+
+<div id="footer_full">
+  <div id="footer">
+    &copy; 2011  All Rights Reserved. Powered by <a href="http://www.wordpress.org">WordPress</a> Design & Developed by <a href="http://www.dessign.net">Dessign.net</a>
+  </div><!--//footer-->
+</div><!--//footer_full-->
+
+<?php wp_footer(); ?>
+<!--%@##--><div style="margin: 1em 0 3em 0; text-align: center;">Find more free <a href="http://www.freewebtemplates.com/wordpress-themes/">WordPress themes</a> at <a href="http://www.freewebtemplates.com/">Free Website Templates</a>.</div><!--##@%-->
+</body>
+</html>
